3edd79555fb720ce8f6d523bce2802a235c24a4e
cloudmojo <cloudmojotech@gmail.com>
2024-09-03 18:52:50 +0530

    fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8ba1372252f471a9"; }
+/******/ 		__webpack_require__.h = function() { return "bcc66bb02ee04f08"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
